b3bf0c6a98aa82a30d4bb826f7b3a4965e79904e
Taha Khan <Taha Khan>
2017-04-12 11:26:27 -0700

    matched ui

--- CS380-EX2/Ex2Client.java
+++ CS380-EX2/Ex2Client.java
@@ -8,45 +8,57 @@ public final class Ex2Client {
 	public static void main(String[] args) throws Exception {
 
 		Socket socket = new Socket("www.codebank.xyz", 38102);
+		System.out.println("Connected to Server.");
 		PrintStream out = new PrintStream(socket.getOutputStream(), true, "UTF-8");
 		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		CRC32 crc = new CRC32();
 		int received = 0;
 		int nibbles[] = new int[200];
-
-		for(int i = 0; i < 200;i++) {
+		System.out.println("Received Bytes:");
+		System.out.print("\t");
+		for (int i = 0; i < 200; i++) {
 			received = in.read();
 			nibbles[i] = received;
-			System.out.print(Integer.toHexString(received));
-			if(i%2 != 0){
-				int byte1 = (nibbles[i-1]*0x10)+nibbles[i];
+			System.out.print(Integer.toHexString(received).toUpperCase());
+			if (i > 0 && i < 199 && (i + 1) % 20 == 0) {
+				System.out.print("\n\t");
+			} else if (i == 199) {
+				System.out.println();
+			}
+			if (i % 2 != 0) {
+				int byte1 = (nibbles[i - 1] * 0x10) + nibbles[i];
 
 				crc.update(byte1);
 			}
 		}
-		
-		System.out.println();
-		
 
-		System.out.println("CRC Value that'll be sent: " + Long.toHexString(crc.getValue()));
-		
+		System.out.println("Gererated CRC32: " + Long.toHexString(crc.getValue()).toUpperCase());
+
+		long crcVal = crc.getValue();
+		byte[] toSend = new byte[4];
+		toSend[0] = (byte) ((byte) crcVal >> 24);
+		toSend[1] = (byte) ((byte) crcVal >> 16);
+		toSend[2] = (byte) ((byte) crcVal >> 8);
+		toSend[3] = (byte) crcVal;
+
 		String crc1 = Long.toHexString(crc.getValue());
-		String[] bytes4 = {crc1.substring(0, 2),crc1.substring(2, 4),crc1.substring(4, 6),crc1.substring(6, 8)};
-		
 
-		
-		for(int i = 0; i < 4; i++){
-			out.println(bytes4[i]);
-			System.out.println("Sent: " + bytes4[i]);
+		for (int i = 0; i < 4; i++) {
+			out.println(toSend[i]);
+			System.out.println("Sent: " + toSend[i]);
 		}
-		int outputFromServer = in.read();
-		System.out.println(outputFromServer);
+		byte outputFromServer = (byte) in.read();
+		if (outputFromServer == 1) {
+			System.out.println("Response Good.");
+		} else {
+			System.out.println("Response Bad.");
+		}
 		in.close();
 		out.close();
 		socket.close();
+		System.out.println("Disconnected from server.");
 		System.exit(0);
 
 	}
-
 
 }
